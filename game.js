5bf5d27847392dc9370eed687046df344bf831c7
tadeuasarro <tadeutas@gmail.com>
2020-12-21 15:12:52 -0300

    Add random size feature

--- game.js
+++ game.js
@@ -4,12 +4,38 @@ let width = 0;
 const gameScreen = () => {
   height = window.innerHeight;
   width = window.innerWidth;
-
-  console.log(height, width);
-}
+};
 
 gameScreen();
 
 window.addEventListener('resize', () => {
   gameScreen();
-})
+});
+
+const randomPos = () => {
+
+  let posX = Math.floor(Math.random() * width) - 90;
+  let posY = Math.floor(Math.random() * height) - 90;
+
+  posX = (posX < 0 ? 0 : posX);
+  posY = (posY < 0 ? 0 : posY);
+
+  const mosquitoe = document.createElement('img');
+  mosquitoe.src = "img/mosca.png";
+  mosquitoe.classList.add('mosquitoe' + randomSize());
+  mosquitoe.style.left = posX + 'px';
+  mosquitoe.style.top = posY + 'px';
+
+  document.body.appendChild(mosquitoe);
+
+};
+
+window.addEventListener('load', () => {
+  randomPos();
+});
+
+const randomSize = () => {
+  return(Math.ceil(Math.random() * 3));
+};
+
+randomSize();
